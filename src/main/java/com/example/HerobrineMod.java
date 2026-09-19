8fa433a5e74f6b2320b0f0152b6ad20cebb632dd
DEVILmihirYT <mr20091011@gmail.com>
2026-09-19 20:17:29 +0530

    Register Herobrine manager

--- src/main/java/com/example/HerobrineMod.java
+++ src/main/java/com/example/HerobrineMod.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.herobrine.HerobrineManager;
 import com.example.herobrine.entity.ModEntityTypes;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -13,5 +14,6 @@ public class HerobrineMod implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Herobrine AI is initializing...");
         ModEntityTypes.registerModEntityTypes();
+        HerobrineManager.initialize();
     }
 }
