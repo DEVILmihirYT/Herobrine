1752f734c480312e8f997e4bf0235be2fd79824d
DEVILmihirYT <mr20091011@gmail.com>
2026-09-22 19:01:46 +0530

    Fix Minecraft 26.1 BlockBehaviour package

--- src/main/java/com/example/herobrine/block/ModBlocks.java
+++ src/main/java/com/example/herobrine/block/ModBlocks.java
@@ -11,7 +11,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Blocks;
 
